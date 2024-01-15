743fea0a22f215e11785186270c2056d69ed3f7c
Alvinsa1 <149544261+Alvinsa1@users.noreply.github.com>
2024-01-15 14:15:52 +0700

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import react from 'react';
+import{View, Text, Image, TextInput, Button, Linking, ScrollView} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+const contactme ='https://instagram.com/alvnrkn_?igshid=MzRlODBiNWFlZA==';
+
+
+const App = () => {
+  
+  return(
+    <View style={{backgroundColor:'#FFFDD0', padding:'auto', alignItems:'center'}}>
+        <Image 
+        style={{width:250, height:200, borderRadius:20, margin:20}} 
+        source={require('./assets/Alvin.jpeg')}/>
+        <Text style={{fontWeight:'bold', fontSize:'1000',textAlign:'center', color:'black'}}>Alvinsa Hafizh Arkaan</Text>
+        <Text style={{fontWeight:'bold', textAlign:'center', color:'gray',padding:20}}> Haiya Oe Alvinsa Hafizh Arkaan seorang siswa SMK TELKOM Purwokerto dan passion saya bergerak dibidang UL/UX Design</Text>
+        <Button title="Kunjungi Instagram" color='' onPress={() => Linking .openURL(contactme)}/>
+
+        <Text style={{padding:20,fontWeight:'bold', fontSize:'500',textAlign:'center', color:'black'}}>My Recent Project</Text>
+        <Image style={{width:300, height:200, borderRadius:10}} source={require('./assets/FigmaApk.png')}/>
+        <Text style={{fontWeight:'Bold', fontSize:'500',textAlign:'left', color:'black'}}>Figma Design</Text>  
+        <Text style={{fontWeight:'medium', fontSize:'500',textAlign:'left', color:'gray'}}>Web Design</Text>  
+        <Image style={{width:300, height:200, borderRadius:10}} source={require('./assets/design.png')}/>
+        <Text style={{fontWeight:'Bold', fontSize:'500',textAlign:'left', color:'black'}}>Desain gatau</Text>
+        <Text style={{fontWeight:'medium', fontSize:'500',textAlign:'left', color:'gray'}}>Web Design</Text>  
     </View>
-  );
+   
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
